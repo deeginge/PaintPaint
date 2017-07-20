abb79ed4c3b3d0c7ef76cbcce6baacee14c65982
deeginge <d.draveler@gmail.com>
2017-07-20 14:07:48 -0600

    Base code working

--- app/src/main/java/club/fdr/paintpaint/MainActivity.java
+++ app/src/main/java/club/fdr/paintpaint/MainActivity.java
@@ -1,7 +1,11 @@
 package club.fdr.paintpaint;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Display display = getWindowManager().getDefaultDisplay();
+
+        int width = display.getWidth(), height = display.getHeight();
+        Bitmap.Config conf = Bitmap.Config.ARGB_8888;
+        Bitmap bm = Bitmap.createBitmap(width, height, conf);
+
+        Canvas canvas = new Canvas(bm);
+        Paint p = new Paint();
+        p.setColor(0xff00ff);
+        canvas.drawCircle(canvas.getWidth()/2, canvas.getHeight()/2, 50, p );
+
+
     }
 }
