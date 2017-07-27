a3391c2efa4cfa657196c13dee55a757f0cc4093
deeginge <d.draveler@gmail.com>
2017-07-27 14:33:25 -0600

    Sepia toner code written but not implemented fully yet

---
+++ app/src/main/java/club/fdr/paintpaint/createSepiaToningEffect.java
@@ -0,0 +1,56 @@
+package club.fdr.paintpaint;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+
+/**
+ * Created by Alex Fritz on 7/24/2017.
+ */
+
+public class createSepiaToningEffect {
+    public static Bitmap createSepiaToningEffect(Bitmap src, int alpha, double red, double green, double blue) {
+        // image size
+        int width = src.getWidth();
+        int height = src.getHeight();
+        // create output bitmap
+        Bitmap bmOut = Bitmap.createBitmap(width, height, src.getConfig());
+        // constant grayscale
+        final double GS_RED = 0.3;
+        final double GS_GREEN = 0.59;
+        final double GS_BLUE = 0.11;
+        // color information
+        int A, R, G, B;
+        int pixel;
+
+        // scan through all pixels
+        for(int x = 0; x < width; ++x) {
+            for(int y = 0; y < height; ++y) {
+                // get pixel color
+                pixel = src.getPixel(x, y);
+                // get color on each channel
+                A = Color.alpha(pixel);
+                R = Color.red(pixel);
+                G = Color.green(pixel);
+                B = Color.blue(pixel);
+                // apply grayscale sample
+                B = G = R = (int)(GS_RED * R + GS_GREEN * G + GS_BLUE * B);
+
+                // apply intensity level for sepid-toning on each channel
+                R += (alpha * red);
+                if(R > 255) { R = 255; }
+
+                G += (alpha * green);
+                if(G > 255) { G = 255; }
+
+                B += (alpha * blue);
+                if(B > 255) { B = 255; }
+
+                // set new pixel color to output image
+                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
+            }
+        }
+
+        // return final image
+        return bmOut;
+    }
+}
